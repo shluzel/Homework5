b57829630bb65b477656caefbeb5e0e591fb3f19
Darya <dasha.890@yandex.ru>
2023-01-23 18:27:29 +0500

    Задание 1

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+void ShowArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+int CountTwo (int [] array) {
+    int count = 0;
+    foreach (var item in array){
+        if (item%2 == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int [] RandomArray = CreateRandomArray(10,100,999);
+Console.WriteLine (String.Join(" ", RandomArray));
+Console.WriteLine ($"Количество четных элементов в массиве: {CountTwo(RandomArray)}");
+
+//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+//Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
